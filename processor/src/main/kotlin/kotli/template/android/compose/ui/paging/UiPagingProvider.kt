91d9f6843924ae3ccedeb5589b758efb0f93dc56
Aleksha <kotlitecture@gmail.com>
2024-04-26 15:00:34 +0300

    refac (*) change processor type

--- processor/src/main/kotlin/kotli/template/android/compose/ui/paging/UiPagingProvider.kt
+++ processor/src/main/kotlin/kotli/template/android/compose/ui/paging/UiPagingProvider.kt
@@ -1,12 +1,15 @@
 package kotli.template.android.compose.ui.paging
 
+import kotli.engine.BaseFeatureProvider
 import kotli.engine.FeatureProcessor
-import kotli.template.android.compose.dataflow.BaseDataFlowProvider
+import kotli.engine.FeatureType
+import kotli.engine.model.FeatureTypes
 import kotli.template.android.compose.ui.paging.jetpack.JetpackComposePagingProcessor
 
-object UiPagingProvider : BaseDataFlowProvider() {
+object UiPagingProvider : BaseFeatureProvider() {
 
     override fun getId(): String = "ui.paging"
+    override fun getType(): FeatureType = FeatureTypes.UI
     override fun createProcessors(): List<FeatureProcessor> = listOf(
         JetpackComposePagingProcessor
     )
